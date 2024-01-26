e02fb7367e585d78dc8ac7d5d5089b8f2029970e
Stoned Fox <oleg.v.zhukov@gmail.com>
2024-01-26 16:42:59 +0700

    Fix tabstops

--- sources/cuda.hpp
+++ sources/cuda.hpp
@@ -32,11 +32,11 @@
 
 #define CALL_CUDA(Func, ...) \
 do {\
-	cudaError_t ec = Func(__VA_ARGS__);\
-	if (ec != cudaSuccess) {\
-		std::cerr << "\033[31mFAILED\033[0m: " << cudaGetErrorString(ec) << std::endl;\
-		return R(CudaError);\
-	}\
+    cudaError_t ec = Func(__VA_ARGS__);\
+    if (ec != cudaSuccess) {\
+        std::cerr << "\033[31mFAILED\033[0m: " << cudaGetErrorString(ec) << std::endl;\
+        return R(CudaError);\
+    }\
 }while(0)
 
 
@@ -45,43 +45,43 @@ namespace ts {;
 
 static Rcode setup_cuda(std::ostream& los) {
 
-	int nbDevices;
-	CALL_CUDA(cudaGetDeviceCount, &nbDevices);
+    int nbDevices;
+    CALL_CUDA(cudaGetDeviceCount, &nbDevices);
 
-	APPEND_INFO(los, "Number of CUDA devices: %d", nbDevices);
+    APPEND_INFO(los, "Number of CUDA devices: %d", nbDevices);
 
-	if (0 == nbDevices) {
-		APPEND_ERROR(los, "No CUDA devices!");
-		return R(CudaSetup);
-	}
+    if (0 == nbDevices) {
+        APPEND_ERROR(los, "No CUDA devices!");
+        return R(CudaSetup);
+    }
 
-	CALL_CUDA(cudaSetDevice, 0);
+    CALL_CUDA(cudaSetDevice, 0);
 
-	APPEND_INFO(los, "Successfully set device 0");
-	return R(Ok);
+    APPEND_INFO(los, "Successfully set device 0");
+    return R(Ok);
 }
 
 
 static Rcode teardown_cuda(std::ostream& los) {
 
-	CALL_CUDA(cudaDeviceReset);
-	APPEND_INFO(los, "Successfully reset device");
-	return R(Ok);
+    CALL_CUDA(cudaDeviceReset);
+    APPEND_INFO(los, "Successfully reset device");
+    return R(Ok);
 }
 
 
 template < typename T >
 struct cuda_array_raii_t {
-	T* arr;
+    T* arr;
 
     cuda_array_raii_t() noexcept : arr(nullptr) {}
-	cuda_array_raii_t(T* a) noexcept : arr(a) {}
+    cuda_array_raii_t(T* a) noexcept : arr(a) {}
    ~cuda_array_raii_t() noexcept { if (arr != nullptr) cudaFree(arr); }
 
-	void release(bool needFree=false) noexcept {
-		if (needFree && !!arr) cudaFree(arr);
-		arr = nullptr;
-	}
+    void release(bool needFree=false) noexcept {
+        if (needFree && !!arr) cudaFree(arr);
+        arr = nullptr;
+    }
 };
 
 
